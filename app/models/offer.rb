d918250125833b27c83678a7ce505ca77007717c
Thibaud Garnier <thibaud.garnier@yahoo.fr>
2019-11-18 18:25:55 +0100

    Models

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :user
   belongs_to :game
+  validates :title, uniqueness: true, presence: true
+  validates :description, uniqueness: true, presence: true
 end
