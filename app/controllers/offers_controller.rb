3c90949c223fbbf668b177cabb6bb05d1bb4fb27
Adrien Peres <adrien.peres@outlook.fr>
2019-11-19 18:55:02 +0100

    Day 2 successless

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,10 +1,25 @@
 class OffersController < ApplicationController
+  before_action :set_offer
   def index
   end
 
   def show
+    @offer = Offer.all.find { |offer| offer['id'] == params[:id].to_i }
   end
 
   def update
+    # @offer = Offer.all.find(params[:id])
+    # if @offer.update(offer_params)
+    # end
   end
+
+  private
+
+  def set_offer
+    @offer = Offer.find(params[:id])
+  end
+
+  # def offer_params
+  #   params.require(:offer).permit
+  # end
 end
