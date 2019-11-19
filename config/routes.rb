f3104cf8c936971dd14cfc96492c26eb489a0b42
Adrien Peres <adrien.peres@outlook.fr>
2019-11-19 16:44:49 +0100

    restart

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'offers/index', to: 'offers#index'
-  get 'offers/show', to: 'offers#show'
-  devise_for :users
+  get 'offers/show/:id/user_id', to: 'offers#show'
+  # devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
