9c5f2811d14bef96647e7b97f81b916cf44ab86b
Thibaud Garnier <thibaud.garnier@yahoo.fr>
2019-11-20 15:00:28 +0100

    offers#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'offers#index'
-​
-  resources :offers, only: [ :show, :update ]
-​
-  namespace :my do
-    resources :offers, only: :index
-  end
-​
-  devise_for :users
-​
+   root to: 'offers#index'
+   resources :offers, only: [ :show, :update ]
+# ​
+   namespace :my do
+     resources :offers, only: :index
+   end
+# ​
+   devise_for :users
+# ​
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
