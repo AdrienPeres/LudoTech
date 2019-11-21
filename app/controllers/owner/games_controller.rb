2869c6fa645b2773f6f2750bd1135e98711eb83d
Marie Begue <marie.prandstetter@gmail.com>
2019-11-21 14:31:01 +0100

    btn delete and add

--- app/controllers/owner/games_controller.rb
+++ app/controllers/owner/games_controller.rb
@@ -7,7 +7,7 @@ class Owner::GamesController < ApplicationController
     @game = Game.new(game_params)
     @game.user = current_user
     if @game.save
-      redirect_to owner_games_index
+      redirect_to owner_games_path
     else
       render :new
     end
@@ -17,9 +17,32 @@ class Owner::GamesController < ApplicationController
     @game = Game.new
   end
 
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to owner_games_path
+    # rattachée au btn delete = à faire depuis l'html de l'index?
+
+    # BTN DELETE <%= link_to "Delete", game_path(game),
+    #   method: :delete,
+    #   data: { confirm: "Supprimer?" } %>
+  end
+
+  def update
+    if @game.update(game_params)
+      redirect_to owner_games_index_path(@game)
+    else
+      render :edit
+    end
+    # rattachée au btn update
+    # <%= link_to "Update", game_path(game),
+    #   method: :update,
+    #   data: { confirm: "Update?" } %>
+  end
+
   private
 
   def game_params
-    params.require(:game).validate(:name)
+    params.require(:game).permit(:name, :description, :photo)
   end
 end
