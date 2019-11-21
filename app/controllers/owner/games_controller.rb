f59631347b06eaaba987bb25a4e233013a4c7185
Marie Begue <marie.prandstetter@gmail.com>
2019-11-21 15:28:30 +0100

    tous les boutons ok

--- app/controllers/owner/games_controller.rb
+++ app/controllers/owner/games_controller.rb
@@ -21,28 +21,24 @@ class Owner::GamesController < ApplicationController
     @game = Game.find(params[:id])
     @game.destroy
     redirect_to owner_games_path
-    # rattachée au btn delete = à faire depuis l'html de l'index?
+  end
 
-    # BTN DELETE <%= link_to "Delete", game_path(game),
-    #   method: :delete,
-    #   data: { confirm: "Supprimer?" } %>
+  def edit
+    @game = Game.find(params[:id])
   end
 
   def update
+    @game = Game.find(params[:id])
     if @game.update(game_params)
-      redirect_to owner_games_index_path(@game)
+      redirect_to owner_games_path(@game)
     else
       render :edit
     end
-    # rattachée au btn update
-    # <%= link_to "Update", game_path(game),
-    #   method: :update,
-    #   data: { confirm: "Update?" } %>
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:name, :description, :photo)
+    params.require(:game).permit(:name, :description)
   end
 end
