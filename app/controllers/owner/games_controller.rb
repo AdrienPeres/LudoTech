f56d66a7b23347475414ff54be78f2fedf9a1ef7
Marie Begue <marie.prandstetter@gmail.com>
2019-11-21 11:52:32 +0100

    controller added

--- app/controllers/owner/games_controller.rb
+++ app/controllers/owner/games_controller.rb
@@ -3,11 +3,23 @@ class Owner::GamesController < ApplicationController
     @games = Game.where(user: current_user)
   end
 
-  def new
-    # TO DO
+  def create
+    @game = Game.new(game_params)
+    @game.user = current_user
+    if @game.save
+      redirect_to owner_games_index
+    else
+      render :new
+    end
   end
 
-  def create
-    # TO DO
+  def new
+    @game = Game.new
+  end
+
+  private
+
+  def game_params
+    params.require(:game).validate(:name)
   end
 end
